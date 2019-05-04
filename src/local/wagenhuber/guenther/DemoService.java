72d25ba2c92319c297d96fa1332dceb335d8b46f
wagenhuber <wagenhuber@gmx.net>
2019-05-04 20:49:26 +0200

    implemented class DemoService and calss TestClient

---
+++ src/local/wagenhuber/guenther/DemoService.java
@@ -0,0 +1,37 @@
+package local.wagenhuber.guenther;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Date;
+import java.util.Vector;
+
+public class DemoService {
+
+    public String getEcho(String text) {
+        return text;
+    }
+
+    public int Summe(Integer x, Integer y) {
+        return x + y;
+    }
+
+    public Date getDate() {
+        return new Date();
+    }
+
+    public void sendMessage(String msg) {
+        System.out.println(msg);
+    }
+
+    public Vector<String> getMessages(String file) throws Exception {
+        Vector<String> lines = new Vector<String>();
+
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = in.readLine()) != null) {
+            lines.add(line);
+        }
+        in.close();
+        return lines;
+    }
+}
---
+++ src/local/wagenhuber/guenther/TestClient.java
@@ -0,0 +1,38 @@
+package local.wagenhuber.guenther;
+
+import java.util.Date;
+import java.util.Vector;
+
+public class TestClient {
+
+    public static void main(String[] args) throws Exception{
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        RPCClient rpcClient = new RPCClient(host, port);
+
+        Object[] params1 = {"Hello"};
+        String s = ((String) rpcClient.call("getEcho", params1));
+        System.out.println("getEcho: " + s);
+
+        Object[] params2 = {10, 33};
+        int summe = ((Integer) rpcClient.call("getSumme", params2));
+        System.out.println("getSumme: " + summe);
+
+        Object[] params3 = {};
+        Date date = ((Date) rpcClient.call("getDate", params3));
+        System.out.println("getDate: " + date);
+
+        Object[] params4 = {"Dies ist ein Test..."};
+        rpcClient.call("sendMessage", params4);
+
+        Object[] params5 = {"msg.txt"};
+
+        Vector<String> v = ((Vector<String>) rpcClient.call("getMessages", params5));
+        System.out.println("getMessages: ");
+        for (String msg : v) {
+            System.out.println(msg);
+        }
+
+    }
+}
